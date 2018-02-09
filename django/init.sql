4ff52ad9240a9eaf8f9503545e451794a395f1dc
Longbin@houzz <longbin@houzz.com>
2018-02-09 10:33:03 -0800

    Update init.sql

--- django/init.sql
+++ django/init.sql
@@ -1,3 +1,3 @@
-CREATE USER 'mlusers'@'localhost' IDENTIFIED BY 'mlusers';
+CREATE USER 'durian'@'localhost' IDENTIFIED BY 'mlusers';
 GRANT ALL PRIVILEGES ON * . * TO 'mlusers'@'localhost';
 CREATE DATABASE alpha;
